1b0166497cc530eb182a3bd439c18864a039b5a8
Marc Gijsen <Marc Gijsen>
2017-12-13 15:44:55 +0100

    Made first test

--- test/hello-test.js
+++ test/hello-test.js
@@ -6,14 +6,29 @@ var chould = chai.should();
 chai.use(chaiHttp);
 
 describe('Hello', function () {
-    it('GET /api/v1/hello', function (done) {
+    it('should GET /api/v1/hello correctly', function (done) {
         chai.request(server)
             .get('/api/v1/hello')
             .end(function (err, res) {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
+                res.should.be.json;
+                res.body.should.be.an('array');
                 // res.body
                 done();
             });
-    })
+    });
+
+    it('should handle /api/v1/hello/error correctly', function (done) {
+        chai.request(server)
+            .get('/api/v1/hello/error')
+            .end(function (err, res) {
+                res.should.have.status(500);
+                res.should.be.json;
+                res.body.should.be.an('object');
+                res.body.should.have.property('message');
+                res.body.message.should.equal('HIER TREEDT EEN ERROR OP');
+                // res.body
+                done();
+            });
+    });
 });
